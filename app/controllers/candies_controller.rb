745ff1efe1a4067227b05b6999086a0d0f5360a1
Eden Rohatensky <edenrohatensky@gmail.com>
2017-10-31 17:16:12 -0400

    Defined actions in candy controller

--- app/controllers/candies_controller.rb
+++ app/controllers/candies_controller.rb
@@ -1,2 +1,45 @@
 class CandiesController < ApplicationController
+  before_action :set_candy, only: [:show, :edit, :update, :destroy]
+  # CREATE
+  def new
+    @candy = Candy.new
+  end
+
+  def create
+    @candy = Candy.create(candy_params)
+    redirect_to candy_path(@candy)
+  end
+
+  # READ
+  def show
+  end
+
+  def index
+    @candies = Candy.all
+  end
+
+  # UPDATE
+  def edit
+  end
+
+  def update
+    @candy.update(candy_params)
+    redirect_to candy_path(@candy)
+  end
+
+  # DELETE
+  def destroy
+    @candy.destroy
+    redirect_to candies_path
+  end
+
+  private
+
+  def set_candy
+    @candy = Candy.find(params[:id])
+  end
+
+  def candy_params
+    params.require(:candy).permit(:name, :address, :category, :rating)
+  end
 end
